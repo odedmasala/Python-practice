08ce06aad698c8c1f6e85404f49f02e637ea9af5
odedmasala <odedmasala6@gmail.com>
2022-10-20 14:08:25 +0300

    update

---
+++ python20.10/demo_tow.py
@@ -0,0 +1,4 @@
+
+
+cars = [{"id": 1, "model": Mazda}, {"id": 2, "model": Toyoda}, {"id": 3, "model": BNW}, {
+    "id": 4, "model": Mercedes}, {"id": 5, "model": Mazda}, {"id": 6, "model": Mazda}, ]
